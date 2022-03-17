3083878a224693e1df5cfd61752cba7ac42f6178
priinvest58 <101271866+priinvest58@users.noreply.github.com>
2022-03-17 07:27:57 -0400

    up

--- js/app.js
+++ js/app.js
@@ -166,3 +166,117 @@ priApp.controller('Main2Controller', function(
     }
 
 });
+
+priApp.controller('LoginController', function(
+    $scope,
+    moment,
+    $window,
+    $rootScope,
+    $timeout
+) {
+
+
+    $scope.user = {};
+
+    $scope.login = function() {
+        console.log($scope.user);
+
+        if ($scope.user.username === "admin" || $scope.user.password === "12345") {
+
+            $window.localStorage.setItem("user", "1");
+            $window.location.href = "./dashboard.html";
+
+        } else {
+
+            alert("Invalid username or password");
+
+        }
+
+
+    }
+
+
+    function loadCategoriesFromServer() {
+        try {
+            $rootScope.db.collection('categories').get().then(result => {
+                const data = result.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+
+                $scope.$apply(function() {
+                    $scope.categories = data;
+
+                });
+                console.log('All numbers in collections', data);
+            });
+        } catch (error) {
+
+        }
+    };
+
+    $scope.checkCatItems = function(cat) {
+
+        $window.localStorage.setItem("currentCat", JSON.stringify(cat));
+        console.log(cat);
+        $window.location.href = "./main_cat.html";
+
+    }
+
+
+    $scope.submitForm = function() {
+
+        var guid = createGuid();
+
+        $rootScope.db
+            .collection('categories')
+            .doc(`${guid}`)
+            .set({
+                id: `${ guid}`,
+                name: `${$scope.form.name}`,
+            })
+            .then(() => {
+                $scope.form.name = "";
+                loadNumbersFromServer();
+                alert(`Created!`);
+
+            })
+            .catch(error => {
+                console.error('Error adding document: ', error);
+            });
+
+
+    };
+
+
+
+    function loadProducts() {
+        try {
+            $rootScope.db.collection('products').where("categoryID", "==", $scope.cat.id).get().then(result => {
+                const data = result.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+
+
+
+                $scope.$apply(function() {
+                    $scope.products = data;
+
+                });
+                console.log('All numbers in collections', data);
+            });
+        } catch (error) {
+
+        }
+    };
+
+    function createGuid() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0,
+                v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
+});
